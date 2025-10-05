57a6eafbe558808f17cf2d6d3b494a11369fbc41
Sebastian Jensen <s.nesgaard@gmail.com>
2025-10-05 15:16:06 +0200

    ged attack

--- scenes/ged/ged.gd
+++ scenes/ged/ged.gd
@@ -1,9 +1,14 @@
 extends RigidBody3D
 
 @export var max_search_distance: float = 100
+@export var attack_distance: float = 20
 @export var move_force: float = 10
+@export var attack_force: float = 30
 
 @onready var timer: Timer = $Timer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@onready var attack_force_dir: Vector3 = Vector3(0, 0, 0)
 
 func _search_for_player() -> Node3D:
 	var d_max_sq = max_search_distance * max_search_distance
@@ -25,7 +30,20 @@ func _process(delta: float) -> void:
 	if player == null:
 		return
 	
-	var n = (player.position - position).normalized()
-	apply_impulse(n * move_force)
+	var n = (player.position - position)
+	var d = n.length()
+	if d < attack_distance:
+		animation_player.play("attack")
+		attack_force_dir = n / d
+		set_process(false)
+		await animation_player.animation_finished
+		set_process(true)
+	else:
+		apply_impulse(n * move_force / d)
 		
-		
+func attack_player() -> void:
+	var n = attack_force_dir
+	var d = n.length()
+	apply_impulse(n * attack_force / d)
+	
+	
