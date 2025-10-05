c832062c3e491d23311d1f42f33348e1a15bfe82
Jeppe Johansen <jeppe@j-software.dk>
2025-10-05 22:36:21 +0200

    Tree physics

---
+++ scenes/tree/tree.gd
@@ -0,0 +1,14 @@
+class_name tree
+extends Node3D
+
+func find_player() -> Node3D:
+	for n in get_tree().get_nodes_in_group("player"):
+		if n is Node3D:
+			return n
+	return
+
+func _process(dt: float) -> void:
+	var player = find_player()
+	if player != null:
+		var new_target = Basis.looking_at(player.position - (position + Vector3(0, 2, 0)), Vector3.UP, true)
+		basis = basis.slerp(new_target, 0.95 * dt)
