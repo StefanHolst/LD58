b659f0f16471072d031f77444f4cd352db0e14ac
Sebastian Jensen <s.nesgaard@gmail.com>
2025-10-06 21:11:33 +0200

    lazer cat

---
+++ scenes/lazercat/lazer.gd
@@ -0,0 +1,37 @@
+extends Node3D
+class_name Lazer
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+@onready var timer: Timer = $Timer
+@onready var mesh: MeshInstance3D = $mesh
+
+
+func _process(delta: float) -> void:
+	if not timer.is_stopped():
+		return
+		
+	var body = ray_cast_3d.get_collider()
+	if body is PlayerBody:
+		Resources.remove_pap(1)
+		timer.start()
+	
+	if ray_cast_3d.is_colliding():
+		var p = ray_cast_3d.get_collision_point()
+		var d = global_position.distance_to(p)
+		#mesh.scale = Vector3(1, 1, d)
+
+
+func aim(p: Vector3, dt: float) -> void:
+	var d = p - global_position
+	var new_target = Basis.looking_at(d, Vector3.UP, true)
+	var new_rotation = new_target.get_rotation_quaternion()
+	global_rotation = global_basis.get_rotation_quaternion().slerp(new_rotation, 0.95 * dt).get_euler()
+	scale = Vector3(1, 1, d.length())
+	if ray_cast_3d.is_colliding():
+		var p2 = ray_cast_3d.get_collision_point()
+		var d2 = global_position.distance_to(p2)
+		mesh.scale = Vector3(1, d2 / d.length(), 1)
+		print(d.length(), " what the what ", global_position.distance_to(p2))
+		
+
+	#ray_cast_3d.scale = Vector3(1, 1, p.length())
---
+++ scenes/lazercat/lazercat.gd
@@ -0,0 +1,18 @@
+extends Node3D
+@onready var lazer: Lazer = $Lazer
+@onready var lazer_2: Lazer = $Lazer2
+
+func find_player() -> Node3D:
+	for n in get_tree().get_nodes_in_group("player"):
+		if n is Node3D:
+			return n
+	return
+
+func look_at_player(dt: float) -> void:
+	var player = find_player()
+	if player is Node3D:		
+		lazer.aim(player.global_position, dt)
+		lazer_2.aim(player.global_position, dt)
+
+func _physics_process(dt: float) -> void:
+	look_at_player(dt)
