15991ab725416dcb40e8aa3ffda6957185be6cba
Stefan Holst <stefanholst0@gmail.com>
2025-10-06 14:12:12 +0200

    upgrades baby

--- scenes/guns/pistol/pistol.gd
+++ scenes/guns/pistol/pistol.gd
@@ -6,6 +6,15 @@ var _last_shot: float = 0
 var _can_shoot = true
 var _trigger_released = true
 
+func _ready() -> void:
+	Resources.pap.connect(Callable(self, "_update"))
+
+func _update():
+	fire_rate = 0.5
+	if Resources.pistol_upgrades & 1 != 0:
+		fire_rate /= 2
+
+
 func _input(event: InputEvent) -> void:
 	if (Resources.leftHand and Resources.leftHand.get_parent() == self) or (Resources.rightHand and Resources.rightHand.get_parent() == self):
 		if event is InputEventKey and event.keycode == KEY_F:
@@ -26,6 +35,9 @@ func trigger():
 		_can_shoot = false
 		Resources.remove_pap(1)
 		var projectile = projectile_scene.instantiate()
+		projectile.damage = 1
+		if Resources.pistol_upgrades & 2 != 0:
+			projectile.damage *= 2
 		get_tree().current_scene.add_child(projectile)
 		projectile.spawn($RigidBody3D.global_position, -$RigidBody3D.global_transform.basis.z)
 
--- scenes/inventory/store/upgrade_row.gd
+++ scenes/inventory/store/upgrade_row.gd
@@ -1,6 +1,18 @@
 extends HBoxContainer
 
-func setup(title: String, price: int, owned: bool):
+signal row_pressed(row)
+
+var title = null
+var price: int = 0
+var owned: bool = false
+var enabled: bool = false
+
+func _ready() -> void:
 	$Title.text = title
 	$Price.text = str(price) + "p"
 	$Button.visible = !owned
+	$Button.disabled = !enabled
+	$Button.connect("pressed", Callable(self, "_on_button_pressed"))
+
+func _on_button_pressed():
+	emit_signal("row_pressed", self)
