e2b74155a28fecd8cc836eb0aa41f9e808cd30b1
Sebastian Jensen <s.nesgaard@gmail.com>
2025-10-06 13:36:44 +0200

    poop ko

---
+++ scenes/ko/ko.gd
@@ -0,0 +1,50 @@
+class_name PoopKo
+extends RigidBody3D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var pop_spawn: Node3D = $Pop_spawn
+@export var pop_scene: PackedScene
+@export var poop_speed: float = 30
+@export var attack_distance: float = 30
+var attack_dir: Vector3 = Vector3(0, 0, 0)
+
+func compute_attack_vector() -> void:
+	var player = find_player()
+	if player == null:
+		attack_dir = Vector3(0, 0, 0)
+	
+	attack_dir = pop_spawn.position.direction_to(player.position)
+
+func distance_to_player() -> float:
+	var player = find_player()
+	if player == null:
+		return INF
+	
+	return player.global_position.distance_to(position)
+
+func fire_poop() -> void:
+	var poop = pop_scene.instantiate()
+	if poop is RigidBody3D:
+		poop.linear_velocity = attack_dir * poop_speed
+		poop.freeze = false
+		poop.global_position = pop_spawn.global_position
+	get_tree().root.get_child(1).add_child(poop)
+
+func find_player() -> Node3D:
+	for n in get_tree().get_nodes_in_group("player"):
+		if n is Node3D:
+			return n
+	return
+
+func look_at_player(dt: float) -> void:
+	var player = find_player()
+	if player != null:
+		var new_target = Basis.looking_at(player.position - (position + Vector3(0, 2, 0)), Vector3.UP, true)
+		basis = basis.slerp(new_target, 0.95 * dt)
+
+func _process(dt: float) -> void:
+	look_at_player(dt)
+	if distance_to_player() <= attack_distance and not animation_player.is_playing():
+		animation_player.play("fire")
+		
+	
